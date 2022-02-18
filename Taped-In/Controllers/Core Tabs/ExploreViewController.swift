1f91cffaea5bb85e87f498acf1499d4b0827ba07
Michael Garcia <garciamichael.developer@gmail.com>
2022-02-18 01:28:52 -0700

    Project set up

---
+++ Taped-In/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  Taped-In
+//
+//  Created by Michael Garcia on 2/18/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Taped-In/Controllers/Core Tabs/PostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostViewController.swift
+//  Taped-In
+//
+//  Created by Michael Garcia on 2/18/22.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
